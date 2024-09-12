7370093b0112614a93798d457aa8fbe70bd8ef28
Jim Katz <jimk@tbrc.org>
2024-09-12 11:43:37 -0400

    change keys to unsigned int

--- database/content.sql
+++ database/content.sql
@@ -3,8 +3,8 @@ use `content`;
 
 CREATE TABLE `content`.`image_file_infos` (
   # image information
-   `id`    INTEGER auto_increment PRIMARY KEY,
-  `storage_file` INTEGER COMMENT 'storage.files.id FK',
+   `id`    INTEGER UNSIGNED auto_increment PRIMARY KEY,
+  `storage_file` INTEGER UNSIGNED COMMENT 'storage.files.id FK',
   `image_type` ENUM ('jpg', 'png', 'single_image_tiff', 'jp2', 'raw') NOT NULL,
   `image_mode` ENUM ('1', 'L', 'RGB', 'RGBA', 'CMYK', 'P', 'OTHER') NOT NULL,
   `tiff_compression` ENUM ('raw', 'tiff_ccitt', 'group3', 'group4', 'tiff_lzw', 'tiff_jpeg', 'jpeg', 'tiff_adobe_deflate', 'lzma', 'other') COMMENT 'names are from PIL version 10',
@@ -21,8 +21,8 @@ ALTER TABLE `content`.`image_file_infos` ADD FOREIGN KEY (`storage_file`) REFERE
 
 CREATE TABLE `content`.`pdf_file_infos` (
   # image information
-  `id` INTEGER auto_increment PRIMARY KEY,
-  `storage_file` INTEGER COMMENT 'storage.files.id FK',
+  `id` INTEGER UNSIGNED  auto_increment PRIMARY KEY,
+  `storage_file` INTEGER UNSIGNED COMMENT 'storage.files.id FK',
   `number_of_pages` smallint UNSIGNED COMMENT 'the number of pages',
   `median_nb_chr_per_page` smallint UNSIGNED COMMENT 'the average number of characters in a page',
   `median_nb_images_per_page` smallint UNSIGNED COMMENT 'the average number of images per page',
@@ -32,4 +32,4 @@ CREATE TABLE `content`.`pdf_file_infos` (
   update_time timestamp default CURRENT_TIMESTAMP null on update CURRENT_TIMESTAMP
 
 );
-ALTER TABLE `content`.`pdf_file_infos` ADD FOREIGN KEY (`storage_file_id`) REFERENCES `storage`.`files` (`id`);
+ALTER TABLE `content`.`pdf_file_infos` ADD FOREIGN KEY (`storage_file`) REFERENCES `storage`.`files` (`id`);
--- database/storage.sql
+++ database/storage.sql
@@ -3,7 +3,7 @@ use `storage`;
 
 CREATE TABLE if not exists `storage`.`roots`
 (
-    `id`     INTEGER PRIMARY KEY AUTO_INCREMENT COMMENT 'internal identifier of the root object',
+    `id`     INTEGER UNSIGNED PRIMARY KEY AUTO_INCREMENT COMMENT 'internal identifier of the root object',
     `name`   varchar(32) CHARACTER SET ascii NOT NULL COMMENT 'name of the Storage root (in ASCII), used to fint it on disk (ex: Archive0). The actual disk path depends on the mount points on the servers.',
     `layout` varchar(50) CHARACTER SET ascii NOT NULL COMMENT 'name of the storage layout, ASCII'
 )  COMMENT 'Storage roots (ex: Archive 1)';
@@ -11,12 +11,12 @@ CREATE TABLE if not exists `storage`.`roots`
 CREATE TABLE if not exists `storage`.`objects`
 (
     # storage objects (ex: W123)
-    `id`               INTEGER PRIMARY KEY AUTO_INCREMENT COMMENT 'internal identifier',
+    `id`               INTEGER UNSIGNED PRIMARY KEY AUTO_INCREMENT COMMENT 'internal identifier',
     # TODO: Would be a good idea to make this an FK to a work object, even if it's only the works in this schema
-    `bdrc_id`          varchar(32) CHARACTER SET ascii NOT NULL COMMENT 'the BDRC RID (ex: W22084), unique persistent identifier, ASCII string no longer than 32 characters',
+    `bdrc_id`          varchar(32) NOT NULL COMMENT 'the BDRC RID (ex: W22084), unique persistent identifier, ASCII string no longer than 32 characters',
     `created_at`       timestamp COMMENT 'the timestamp of the creation of the object, or the equivalent object in a previous archive storage',
     `last_modified_at` timestamp COMMENT 'the timestamp of the last known modification',
-    `root`             INTEGER NOT NULL COMMENT 'the OCFL storage root id that the object is stored in'
+    `root`             INTEGER UNSIGNED NOT NULL COMMENT 'the OCFL storage root id that the object is stored in'
 ) COMMENT 'Objects kept on archive storage';
 
 CREATE UNIQUE INDEX `objects_index_0` ON `storage`.`objects` (`bdrc_id`, `root`);
@@ -28,9 +28,9 @@ ALTER TABLE `storage`.`objects`
 CREATE TABLE if not exists `storage`.`files`
 (
     # files
-    `id`               INTEGER PRIMARY KEY AUTO_INCREMENT,
+    `id`               INTEGER UNSIGNED PRIMARY KEY AUTO_INCREMENT,
     `digest`           binary(32)                NOT NULL COMMENT 'the digest of the file',
-    `digest_algorithm` ENUM ('sha256', 'sha512', 'sha1','md5','not_set')DEFAULT 'not_set' COMMENT 'the algorithm used to compute the digest',
+#    `digest_algorithm` ENUM ('sha256', 'sha512', 'sha1','md5','not_set')DEFAULT 'not_set' COMMENT 'the algorithm used to compute the digest',
     `size`             bigint UNSIGNED           NOT NULL COMMENT 'the size in bytes',
     `pronom_number`    smallint UNSIGNED        COMMENT 'the PRONOM number or, if unavailable, null',
     `persistent_id`    binary(32) UNIQUE         NOT NULL COMMENT 'The identifier is globally unique for the BDRC archive. By construction it is the sha256 or a random id in case of collision.',
@@ -44,9 +44,9 @@ CREATE UNIQUE INDEX `files_index_1` ON `storage`.`files` (`digest`, `size`);
 CREATE TABLE if not exists `storage`.`paths`
 (
     # file paths in objects
-    `id`            INTEGER PRIMARY KEY AUTO_INCREMENT,
-    `file`           INTEGER ,
-    `storage_object` INTEGER ,
+    `id`            INTEGER UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+    `file`           INTEGER UNSIGNED ,
+    `storage_object` INTEGER UNSIGNED  ,
     `path`           varchar(1024) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin COMMENT 'Unicode string (256 Unicode characters max) representing the (case sensitive) content paths in OCFL objects for each content file.',
     `image_group`    varchar(32) CHARACTER SET ascii COMMENT 'the BDRC image group RID (ex: I0886), unique persistent identifier, ASCII string no longer than 32 characters, when unknown set to NULL',
     `root_folder`    ENUM ('images', 'archive', 'sources', 'backup', 'eBooks', 'web', 'other') COMMENT 'should be obvious. other can be anything, not just the folder other'
