377f5462c67b1d85f316976dbed793c515daaf91
Jim Katz <jimk@tbrc.org>
2024-10-09 13:55:42 -0400

    dump image data for expected population

--- test/test_ImgMetadata.py
+++ test/test_ImgMetadata.py
@@ -62,12 +62,24 @@ test_source_dir: Path = Path(os.getcwd(), 'sources/')
 def test_image_metadata(source, expected):
 #    try:
     metadata: BaseImage = image_info_factory(source)
-    assert metadata.image_type == expected['image_type']
-    assert metadata.image_mode == expected['image_mode']
-    assert metadata.width == expected['width']
-    assert metadata.height == expected['height']
-    assert metadata.compression == expected['compression']
-    assert metadata.quality == expected['quality']
-    assert metadata.resolution == expected['resolution']
-    assert metadata.recorded_date == expected['recorded_date']
-    assert metadata.modified_date == expected['modified_date']
+    # assert metadata.image_type == expected['image_type']
+    # assert metadata.image_mode == expected['image_mode']
+    # assert metadata.width == expected['width']
+    # assert metadata.height == expected['height']
+    # assert metadata.compression == expected['compression']
+    # assert metadata.quality == expected['quality']
+    # assert metadata.resolution == expected['resolution']
+    # assert metadata.recorded_date == expected['recorded_date']
+    # assert metadata.modified_date == expected['modified_date']
+
+    from pprint import pp
+    pp(f"{metadata.image_path=}")
+    pp(f"{metadata.image_type=}")
+    pp(f"{metadata.image_mode=}")
+    pp(f"{metadata.width=}")
+    pp(f"{metadata.height=}")
+    pp(f"{metadata.compression=}")
+    pp(f"{metadata.quality=}")
+    pp(f"{metadata.resolution=}")
+    pp(f"{metadata.recorded_date=}")
+    pp(f"{metadata.modified_date=}")
