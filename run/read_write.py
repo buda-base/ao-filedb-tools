e2047436f2f9cfdbb25fb898133f5ebe3277297d
Jim Katz <jimk@tbrc.org>
2024-12-20 16:37:28 -0500

    Avoid duplicate key

--- run/read_write.py
+++ run/read_write.py
@@ -6,6 +6,8 @@ import os
 from pathlib import Path
 
 from BdrcDbLib.DbOrm.DrsContextBase import DrsDbContextBase
+from sqlalchemy.dialects.mysql import insert
+
 import image_info as ii
 import FileInfo as fi
 
@@ -37,33 +39,46 @@ def main():
     args = RwArgParser().parse_args()
     src: Path = Path(args.path)
     with DrsDbContextBase(args.content_db) as content_db:
+        # recurse one directory down. No further
         if src.is_dir():
             nlim: int = 0
             print(f"Reading directory {src}")
             for p in src.iterdir():
                 print(f"Reading {str(p)}")
                 _orm_image = read_one(p)
-                _orm_file = fi.f_to_files(p)
-                _orm_image.file = _orm_file
-
-                if not _orm_image:
-                    print(f"Skipping dir entry {str(p)}")
-                    continue
-                else:
-                    content_db.session.add(_orm_image)
+                if process(p,_orm_image, content_db):
                     nlim += 1
-                    if nlim > 10:
-                        content_db.session.commit()
-                        nlim = 0
+                else:
+                    print(f"Skipping file {str(p)}")
+                if nlim > 10:
+                    content_db.session.commit()
+                    nlim = 0
 
         if src.is_file():
-            print(f"Reading file {src}")
             _orm_image = read_one(src)
-            if not _orm_image:
+            if not process(src,_orm_image, content_db):
                 print(f"Skipping file {str(src)}")
-            else:
-                content_db.session.add(_orm_image)
-                content_db.session.commit()
+                # commit happens on exit
+
+
+def process(p,_orm_image, content_db):
+    image_to_file(p, _orm_image)
+    image_to_db(content_db, _orm_image)
+
+def image_to_file(p, orm_image):
+    _orm_file = fi.f_to_files(p)
+    orm_image.file = _orm_file
+
+
+def image_to_db(content_db, orm_image):
+    # Inspect the table dynamically
+    stmt = insert(orm_image.__table__).values(
+        **orm_image.__dict__
+    ).on_duplicate_key_update(
+        **{col.name: getattr(orm_image, col.name) for col in orm_image.__table__.columns}
+    )
+    content_db.session.execute(stmt)
+    content_db.session.add(orm_image)
 
 
 def read_one(p: Path)-> object:
